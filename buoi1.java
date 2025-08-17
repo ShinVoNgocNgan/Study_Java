9c9f740be5e679eaadc4237577f38215c9291caa
Truong Ngan <truongvokimngan13@gmail.com>
2025-08-17 10:12:51 +0700

    buoi2

--- buoi1.java
+++ buoi1.java
@@ -1,7 +1,23 @@
-public class buoi1 {
-    public static void main(String[] args) {
-        System.out.println("Xin chào, Java!");       
-        System.out.println("Xin chào, Java!");
+class buoi1
+{
+    public static void main (String args[])
+ {  
+  int n1 = 45;
+  int n2 = 32;
+  float n3 = 8.765f;
+  double n4 = 1.12345678900000; 
+  int rs = n1 + n2; 
+  boolean b = true;
+  byte by = 125;
+  int a = 257;
+  System.out.println((byte) a);
 
-    }
+  System.out.println(rs);
+  System.out.println(b); 
+  System.out.println(n1);
+  System.out.println(n2);
+  System.out.println(n3);
+  System.out.println(n4);
+  //System.out.println(n5)
+ }
 }
---
+++ buoi2.java
@@ -0,0 +1,21 @@
+class buoi2 { 
+    public static void main (String args[])
+    {
+        // byte b = 5;
+        // b = b + 1;
+        // // b += 1;
+        // System.out.println(b);
+        // int a = 5;
+        // System.out.println(a++); // In ra 5 (giá trị cũ)
+        // System.out.println(a);   // In ra 6 (đã tăng sau đó)
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int d = 8;
+
+        boolean rs = b > a || d > c ;
+        System.out.println (rs);
+
+    
+    }}
+   
